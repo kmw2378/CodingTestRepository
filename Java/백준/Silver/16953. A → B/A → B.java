d2c97328d8f54c9f922dbe554e08bee34e235df0
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-05-21 17:47:02 +0900

    [Silver II] Title: A → B, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ Java/백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int count = 1;
+        while (a != b) {
+            String str = String.valueOf(b);
+            
+            if (b % 2 == 0) {
+                b /= 2;
+            } else if (str.charAt(str.length() - 1) == '1') {
+                b = Integer.parseInt(str.substring(0, str.length() - 1));
+            } else {
+                count = -1;
+                break;
+            }
+            
+            if (b < a) {
+                count = -1;
+                break;
+            }
+            
+            count++;
+        }
+        
+        System.out.println(count);
+    }
+}
