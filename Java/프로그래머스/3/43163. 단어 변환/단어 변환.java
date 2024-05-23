1057f1b47308b384544a041cf7a500b7b5836fc4
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-05-23 22:58:23 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 66.9 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,39 @@
 class Solution {
+    boolean[] visited;
     int answer = Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        dfs(begin, target, words, visited, 1);
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
     
-    private int dfs(String begin, String target, String[] words, boolean[] visited, int count) {
-        if (begin.equals(target)) {
-            answer = count - 1;
+    private void dfs(String current, String target, String[] words, int cnt) {
+        if (current.equals(target)) {
+            answer = Math.min(answer, cnt);
+            return;
         }
         
         for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && diffOneChar(begin, words[i])) {
+            if (!visited[i] && isDifferentOneChar(current, words[i])) {
                 visited[i] = true;
-                answer = Math.min(answer, dfs(words[i], target, words, visited, count + 1));
+                dfs(words[i], target, words, cnt + 1);
                 visited[i] = false;
             }
         }
-        
-        return answer;
     }
     
-    private boolean diffOneChar(String s1, String s2) {
+    private boolean isDifferentOneChar(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
         }
         
-        int count = 0;
+        int cnt = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                count++;
+                cnt++;
             }
         }
         
-        return count == 1;
+        return cnt == 1;
     }
 }
