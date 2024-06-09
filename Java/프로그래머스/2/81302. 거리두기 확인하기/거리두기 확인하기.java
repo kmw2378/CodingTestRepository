4ace7d36f6c72a5f12a95df3087ac50aaf377c2d
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-06-09 22:34:18 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ Java/프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,84 +1,62 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    boolean[][] visited;
+    int[] answer;
+    
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
+        answer = new int[places.length];
+        Arrays.fill(answer, 1);
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = solution(places[i]);
+            solution(i, places[i]);
         }
 
         return answer;
     }
     
-    public int solution(String[] place) {
-        List<int[]> list = new ArrayList<>();
+    public void solution(int seq, String[] place) {
+        visited = new boolean[5][5];
         for (int i = 0; i < place.length; i++) {
             for (int j = 0; j < place[i].length(); j++) {
-                int p = place[i].charAt(j);
-                if (p == 'P') {
-                    list.add(new int[]{i, j});
+                if (!visited[i][j] && place[i].charAt(j) == 'P') {
+                    visited[i][j] = true;
+                    dfs(seq, i, j, 0, place);
+                    visited[i][j] = false;
                 }
             }
         }
-        
-        for (int[] current : list) {
-            for (int[] target : list) {
-                if (current[0] == target[0] && current[1] == target[1]) {
-                    continue;
-                }
-                
-                if (getDistance(current, target) > 2) {
-                    continue;
-                }
-                
-                if (current[0] == target[0]) {
-                    if (getDistance(current, target) == 1) {
-                        return 0;
-                    }
-                    int x = current[0];
-                    int minY = Math.min(current[1], target[1]);
-                    int maxY = Math.max(current[1], target[1]);
-                    for (int y = minY + 1; y < maxY; y++) {
-                        if (place[x].charAt(y) != 'X') {
-                            return 0;
-                        }
-                    }
-                } else if (current[1] == target[1]) {
-                    if (getDistance(current, target) == 1) {
-                        return 0;
-                    }
-                    int y = current[1];
-                    int minX = Math.min(current[0], target[0]);
-                    int maxX = Math.max(current[0], target[0]);
-                    for (int x = minX + 1; x < maxX; x++) {
-                        if (place[x].charAt(y) != 'X') {
-                            return 0;
-                        }
-                    }
-                } else {
-                    int minX = Math.min(current[0], target[0]);// 0
-                    int minY = Math.min(current[1], target[1]);// 1
-                    int maxX = Math.max(current[0], target[0]);// 1
-                    int maxY = Math.max(current[1], target[1]);// 2
-                    
-                    int m = (current[0] - target[0]) / (current[1] - target[1]);
-                    if (m < 0) {
-                         if (place[maxX].charAt(maxY) != 'X' || place[minX].charAt(minY) != 'X') {
-                            return 0;
-                        }                   
-                    } else {
-                        if (place[minX].charAt(maxY) != 'X' || place[maxX].charAt(minY) != 'X') {
-                            return 0;
-                        }
-                    }
-                }
-            }
-        }
-        
-        return 1;
     }
     
-    private int getDistance(int[] a, int[] b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    private void dfs(int seq, int x, int y, int count, String[] place) {
+        if (count > 2) {
+            return;
+        }
+        
+        if (0 < count && count <= 2 && place[x].charAt(y) == 'P') {
+            answer[seq] = 0;
+            return;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) {
+                continue;
+            }
+            
+            if (place[nx].charAt(ny) == 'X') {
+                continue;
+            }
+            
+            if (visited[nx][ny]) {
+                continue;
+            }
+            
+            visited[nx][ny] = true;
+            dfs(seq, nx, ny, count + 1, place);
+            visited[nx][ny] = false;
+        }
     }
 }
