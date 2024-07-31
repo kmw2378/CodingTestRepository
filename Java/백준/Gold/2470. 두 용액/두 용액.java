2f341e89ddeb37d10a3a5a879304c86219dd5fe5
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-07-31 13:47:36 +0900

    [Gold V] Title: 두 용액, Time: 392 ms, Memory: 30292 KB -BaekjoonHub

---
+++ Java/백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(reader.readLine());
+        int[] arr = Arrays.stream(reader.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        
+        int[] result = new Solution().solution(n, arr);
+        System.out.printf("%d %d\n", result[0], result[1]);
+    }
+}
+
+class Solution {
+    public int[] solution(int n, int[] arr) {
+        Arrays.sort(arr);
+        int left = 0;
+        int right = n - 1;
+        int[] result = new int[2];
+        int min = Integer.MAX_VALUE;
+        
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                result[0] = arr[left];
+                result[1] = arr[right];
+                
+                if (sum == 0) {
+                    break;
+                }
+            }
+            
+            if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return result;
+    }
+}
