dc6ee0723f2949de11a664d4ee31104871ffd300
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-08-14 14:26:29 +0900

    [Gold IV] Title: 좋은수열, Time: 132 ms, Memory: 16344 KB -BaekjoonHub

--- Java/백준/Gold/2661. 좋은수열/좋은수열.java
+++ Java/백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,46 +1,37 @@
 import java.io.*;
-import java.util.*;
- 
+
 public class Main {
-    static int length;
-    static char[] arr = {'1', '2', '3'};
-    static boolean flag;
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        length = Integer.parseInt(st.nextToken());
- 
-        backTracking("");
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(reader.readLine());
+        StringBuilder sb = new StringBuilder();
+        backtracking(n, sb);
     }
- 
-    static void backTracking(String str) {
-        if(flag) return;
- 
-        if (str.length() == length ) {
-            flag = true;
-            System.out.print(str);
-        }
- 
-        /*if(str.length() == length){
-            System.out.print(str);
+    
+    static void backtracking(int n, StringBuilder sb) {
+        if (sb.length() == n) {
+            System.out.println(sb.toString());
             System.exit(0);
-            //return;
-        }*/
- 
-        for (int i = 0; i < arr.length; i++) {
-            if (isConditionTrue(str + arr[i])) {
-                backTracking(str + arr[i]);
-            }
         }
- 
- 
+        
+        for (int i = 1; i <= 3; i++) {
+            sb.append(i);
+            if (satisfied(sb)) {
+                backtracking(n, sb);
+            }
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
- 
-    static boolean isConditionTrue(String str) {
-        for (int i = 1; i <= str.length() / 2; i++) {
-            if (str.substring(str.length() - i).equals(str.substring(str.length() - 2 * i, str.length() - i)))
+    
+    static boolean satisfied(StringBuilder sb) {
+        String s = sb.toString();
+        for (int i = 1; i <= s.length() / 2; i++) {
+            String front = s.substring(s.length() - i * 2, s.length() - i);
+            String back = s.substring(s.length() - i);
+            if (front.equals(back)) {
                 return false;
+            }
         }
         return true;
     }
