07928cd69007d3ec8078c227f17afec1dea31637
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-11-20 17:37:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.86 ms, Memory: 54.6 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,44 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {1, -1, 0, 0};
-    int[] dy = {0, 0, 1, -1};
-    public int solution(int[][] maps) {
-        return dijkstra(0, 0, maps);
-    }
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
     
-    private int dijkstra(int x, int y, int[][] maps) {
+    public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
+        int[] start = {0, 0};
+        return dijkstra(maps, start, n, m);
+    }
+    
+    private int dijkstra(int[][] maps, int[] start, int n, int m) {
         Queue<int[]> queue = new LinkedList<>();
         int[][] costs = new int[n][m];
         for (int[] cost : costs) {
-            Arrays.fill(cost, -1);
+            Arrays.fill(cost, Integer.MAX_VALUE);
         }
-        costs[x][y] = 1;
-        queue.add(new int[]{x, y});
+        
+        queue.add(start);
+        costs[start[0]][start[1]] = 1;
         
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
             for (int i = 0; i < 4; i++) {
-                int nx = current[0] + dx[i];
-                int ny = current[1] + dy[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || maps[nx][ny] == 0) {
                     continue;
                 }
                 
-                if (maps[nx][ny] == 1 && costs[nx][ny] == -1) {
-                    costs[nx][ny] = costs[current[0]][current[1]] + 1;
+                if (costs[nx][ny] > costs[x][y] + 1) {
+                    costs[nx][ny] = costs[x][y] + 1;
                     queue.add(new int[]{nx, ny});
                 }
             }
         }
         
-        return costs[n - 1][m - 1];
+        return costs[n - 1][m - 1] == Integer.MAX_VALUE ? -1 : costs[n - 1][m - 1];
     }
 }
